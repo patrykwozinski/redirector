00b0c4dff15fb72b04b544d9cf8245ddc112a198
Patryk Woziński <patryk.wozinski@gmail.com>
2017-08-12 12:26:43 +0200

    Cleanup

--- src/Contracts/StorageInterface.php
+++ src/Contracts/StorageInterface.php
@@ -6,11 +6,11 @@ use Redirector\Contracts\Redirectable;
 
 interface StorageInterface
 {
-	public function get(string $keyword): ?array;
+    public function get(string $keyword): ?array;
 
-	public function store(): void;
+    public function store(): void;
 
-	public function delete(): void;
+    public function delete(): void;
 
-	public function flush(): void;
+    public function flush(): void;
 }
